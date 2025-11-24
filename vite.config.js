37523dab60349eef1efc2c6e9eda669e1ef993b7
yevhenii kytsmen <yevheniikytsmen@gmail.com>
2025-11-24 17:59:59 +0200

    CONFIG: Final Vite adjustments for Vercel and production build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+
+    // Якщо ви хочете завантажити сайт у підпапку на GitHub Pages,
+    // тут має бути рядок: base: "/назва_вашого_репо/"
+
 })
