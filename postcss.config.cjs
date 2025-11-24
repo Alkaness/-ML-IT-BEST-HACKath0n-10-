31e8d6b9f017db6f4dc8b3c7d922b76df129b364
yevhenii kytsmen <yevheniikytsmen@gmail.com>
2025-11-24 19:15:21 +0200

    FIX: Renamed postcss.config.js to .cjs to resolve ES Module scope error on Vercel

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+        // Використовуємо require() для CommonJS, який вимагає PostCSS
+        'tailwindcss': {},
+        'autoprefixer': {},
+    },
+};
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: {
-        // Використовуємо require() для CommonJS, який вимагає Node.js
-        'tailwindcss': {},
-        'autoprefixer': {},
-    },
-};
